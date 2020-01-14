13c8334ccc3027c45aa6e6b4d2facc019a2f5a5c
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-01-15 01:58:49 +0500

    djs

---
+++ precache-manifest.4f1c9794a4845415d12166d4481db038.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c434a62141257f26cc436097bbe1e54",
+    "url": "/my-email/index.html"
+  },
+  {
+    "revision": "d05fc766f0e492d2c817",
+    "url": "/my-email/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "bc19019d7b5c3e8b5730",
+    "url": "/my-email/static/css/main.973917f7.chunk.css"
+  },
+  {
+    "revision": "d05fc766f0e492d2c817",
+    "url": "/my-email/static/js/2.a897da27.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/my-email/static/js/2.a897da27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc19019d7b5c3e8b5730",
+    "url": "/my-email/static/js/main.1597af4f.chunk.js"
+  },
+  {
+    "revision": "b9aeb03baab0aab42843",
+    "url": "/my-email/static/js/runtime-main.0b730464.js"
+  },
+  {
+    "revision": "f28cd6fa964792a2f3708f4b29d8b46c",
+    "url": "/my-email/static/media/bgimage.f28cd6fa.jpg"
+  }
+]);
